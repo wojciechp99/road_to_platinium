0f4316e9956b94a9ecb7e619755871ed90fb9912
Wojciech <wojciechp99@outlook.com>
2021-12-08 02:33:25 +0100

    -added achievement view and update view in urls.py and views.py -added forms.html -added games_all_achiev.html

--- games/achievements/urls.py
+++ games/achievements/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
-from .views import games
+from .views import games, GameAchievementsView, AchievementUpdateView
 
 urlpatterns = [
-    path('', games, name='index')
+    path('game/<int:pk>', GameAchievementsView.as_view(), name="achiev"),
+    path('achiev/update/<int:pk>', AchievementUpdateView.as_view(), name='achiev-update'),
+    path('', games, name='index'),
 ]
--- games/achievements/views.py
+++ games/achievements/views.py
@@ -1,5 +1,21 @@
 from django.shortcuts import render
+from django.urls import reverse_lazy
+from django.views.generic import ListView, UpdateView
+
 from .models import Game, Achievement
+
+
+class AchievementUpdateView(UpdateView):
+    template_name = 'forms.html'
+    model = Achievement
+    fields = '__all__'
+    success_url = reverse_lazy('index')
+
+
+class GameAchievementsView(ListView):
+    template_name = 'games_all_achiev.html'
+    model = Achievement
+    context_object_name = 'achievements'
 
 
 def games(request):
