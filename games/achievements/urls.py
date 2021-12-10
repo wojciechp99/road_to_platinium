9e4eb21882d5de8ad35ac4b0868c2619cba391d6
Wojciech <wojciechp99@outlook.com>
2021-12-10 02:08:16 +0100

    Added function that allows to get achievements from steam api

--- games/achievements/urls.py
+++ games/achievements/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path
-from .views import games, GameAchievementsView, AchievementUpdateView, change_achievement_status, AchievementDetailView
+from .views import games, GameAchievementsView, AchievementUpdateView, change_achievement_status, AchievementDetailView, \
+    upload_to_database
 
 urlpatterns = [
     path('game/<int:pk>', GameAchievementsView.as_view(), name="achiev"),
     path('achiev/update/<int:pk>', AchievementUpdateView.as_view(), name='achiev-update'),
     path('<int:pk>/change-achievement-status', change_achievement_status, name='change-status'),
     path('achiev/<int:pk>', AchievementDetailView.as_view(), name='achiev-detail'),
+    path("api/upload-achievements", upload_to_database, name='upload-with-api'),
     path('', games, name='index'),
 ]
--- games/achievements/views.py
+++ games/achievements/views.py
@@ -1,4 +1,4 @@
-from django.http import HttpResponseRedirect
+from django.http import HttpResponseRedirect, HttpRequest
 from django.shortcuts import render
 from django.urls import reverse_lazy
 from django.views.generic import ListView, UpdateView, DetailView
@@ -38,10 +38,39 @@ def change_achievement_status(request, pk):
     return HttpResponseRedirect(reverse_lazy('achiev', kwargs={'pk': game.id}))
 
 
+def upload_to_database(request):
+    from django.utils.text import slugify
+    import requests
+
+    STEAM_KEY = "0"  # for now key is stored as value
+    GAME_ID = "211420"  # page for obtaining game id: https://steamdb.info/ , for now also stored as value
+
+    get_api = requests.get(
+        f"https://api.steampowered.com/ISteamUserStats/GetSchemaForGame/v0002/?key={STEAM_KEY}&appid={GAME_ID}&l=english&format=json")
+    achievements = (get_api.json()['game']["availableGameStats"]["achievements"])
+    game = Game.objects.get(name="Dark Souls")
+
+    for index in achievements:
+        if Achievement.objects.get(name=index["displayName"]):
+            continue
+
+        if index['hidden'] == 0:
+            description = index['description']
+        else:
+            description = "Description for this achievement is hidden"
+
+        Achievement.objects.create(name=index["displayName"],
+                                   game=game,
+                                   link=index['icon'],
+                                   description=description,
+                                   slug=slugify(index["displayName"]))
+
+    return render(request, template_name='games_all_achiev.html', context={'achievements': Achievement.objects.all()})
+
+
 def games(request):
     return render(request, template_name='games.html',
                   context={'games': Game.objects.all(),
                            'achievements': Achievement.objects.all().count(),
                            'finished': Achievement.objects.all().filter(completed=True).count()
                            })
-
